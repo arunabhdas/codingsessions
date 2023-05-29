e70fd75889a19e8c616114b7390e1e6b5b8390c2
arunabhdas <arunabhdas@gmail.com>
2023-05-29 13:41:30 -0400

    Added method

--- python/inheritance.py
+++ python/inheritance.py
@@ -11,6 +11,16 @@ class Programmer(Person):
         super().__init__(firstname, lastname)
         self.favelanguage = favelanguage
 
-    def printname(self):
+    def printname_and_language(self):
         print(f'{self.firstname} {self.lastname} loves {self.favelanguage}')
 
+
+
+if __name__ == '__main__':
+    p1 = Person('John', 'Doe')
+    p1.printname()
+
+    p2 = Programmer('Jane', 'Doe', 'Python')
+    p2.printname_and_language()
+    
+     
